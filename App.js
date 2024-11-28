3a957b5ea2fc4ff8e25263b0a005f9c2e77aacd4
Lutz Dietterich <lutzdietterich@googlemail.com>
2024-11-28 17:03:32 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,159 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useRef } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  Image,
+  Animated,
+  Easing,
+} from "react-native";
 
-export default function App() {
+const App = () => {
+  const currentTime = new Date().toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  const currentDate = new Date().toLocaleDateString("de-DE", {
+    weekday: "long",
+    day: "2-digit",
+    month: "long",
+  });
+
+  const rotateValue = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    const rotateAnimation = () => {
+      rotateValue.setValue(0);
+      Animated.loop(
+        Animated.timing(rotateValue, {
+          toValue: 1,
+          duration: 2000,
+          easing: Easing.linear,
+          useNativeDriver: true,
+        })
+      ).start();
+    };
+    rotateAnimation();
+  }, [rotateValue]);
+
+  const rotation = rotateValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0deg", "360deg"],
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      source={require("./assets/background.jpg")}
+      style={styles.background}
+    >
+      <View style={styles.container}>
+        <Text style={styles.time}>{currentTime}</Text>
+        <Text style={styles.date}>{currentDate}</Text>
+
+        <View style={styles.dataContainer}>
+          <View style={styles.dataColumn}>
+            <Text style={styles.dataTitle}>IN</Text>
+            <Image
+              source={require("./assets/temperatur.png")}
+              style={styles.icon}
+            />
+            <Text style={styles.dataValue}>20°C</Text>
+
+            <Image source={require("./assets/wind.png")} style={styles.icon} />
+            <Text style={styles.dataValue}>50%</Text>
+          </View>
+
+          <View style={styles.dataCenter}>
+            <Animated.Image
+              source={require("./assets/fan.png")}
+              style={[styles.largeIcon, { transform: [{ rotate: rotation }] }]}
+            />
+            <Text style={styles.autoText}>AUTO</Text>
+            <Image
+              source={require("./assets/clock.png")}
+              style={styles.largeIcon}
+            />
+          </View>
+
+          <View style={styles.dataColumn}>
+            <Text style={styles.dataTitle}>OUT</Text>
+            <Image
+              source={require("./assets/temperatur.png")}
+              style={styles.icon}
+            />
+            <Text style={styles.dataValue}>20°C</Text>
+
+            <Image source={require("./assets/wind.png")} style={styles.icon} />
+            <Text style={styles.dataValue}>50%</Text>
+          </View>
+        </View>
+      </View>
+    </ImageBackground>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  container: {
+    alignItems: "center",
+  },
+  time: {
+    fontSize: 64,
+    color: "white",
+  },
+  date: {
+    fontSize: 20,
+    color: "white",
+    marginBottom: 30,
+  },
+  dataContainer: {
+    flexDirection: "column",
+    alignItems: "center",
+    marginTop: 30,
+  },
+  dataColumn: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginHorizontal: 20,
+    marginVertical: 20,
+    paddingHorizontal: 20,
+  },
+  dataTitle: {
+    fontSize: 30,
+    color: "white",
+    marginBottom: 5,
+    paddingHorizontal: 20,
+  },
+  dataValue: {
+    fontSize: 30,
+    color: "white",
+    paddingHorizontal: 10,
+  },
+  dataCenter: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginHorizontal: 20,
+    gap: 20,
+  },
+  autoText: {
+    fontSize: 18,
+    color: "lime",
+    marginVertical: 10,
+  },
+  icon: {
+    width: 24,
+    height: 24,
+    tintColor: "white",
+  },
+  largeIcon: {
+    width: 50,
+    height: 50,
   },
 });
+
+export default App;
